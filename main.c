9d1f349f05c8afa69b0e9ba0fa6dae70c8b51dda
Mirko Morati <mirkomorati@gmail.com>
2017-05-11 11:05:17 +0200

    Creata una struttura dati per una matrice, cominciamo a fare file separati? Adattato il codice di conseguenza. Problema nel parsing. Tanti printf perché non ho voglia di usare gdb.

--- main.c
+++ main.c
@@ -5,14 +5,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdarg.h>
 #include <fcntl.h>
 #include <getopt.h>
 #include <string.h>
+#include <signal.h>
 #include <sys/shm.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 
 #define BUF_SIZE 4096
+
+struct Matrix {
+    char *path;
+    int shmid;
+    key_t key;
+    int fd;
+    long *shmaddr;
+};
 
 static struct option long_options[] = {
     /* These options don’t set a flag.
@@ -27,65 +37,85 @@ static struct option long_options[] = {
 };
 
 
-void parse_matrix(long int *addr, int fd) {
+void parse_matrix(struct Matrix *M) {
     char buf[BUF_SIZE];
     int i = 0;
+    char *line, *value, *brkt, *brkb;
 
-    while (read(fd, buf, BUF_SIZE) != EOF) {
-        char *line;
+    printf("---PARSING %s\n", M->path);
+    while (read(M->fd, buf, BUF_SIZE) != EOF) {
 
-        for (line = strtok(buf, "\n"); line; line = strtok(NULL, "\n")) {
-            char *value;
-            
-            for (value = strtok(line, ";"); value; value = strtok(NULL, ";")) {
-                addr[i] = atol(value);    
+        for (line = strtok_r(buf, "\n", &brkt); line; line = strtok_r(NULL, "\n", &brkt)) {
+
+            for (value = strtok_r(line, ";", &brkb); value; value = strtok_r(NULL, ";", &brkb)) {
+                M->shmaddr[i] = atol(value);    
+                printf("%i: %li\t", i, M->shmaddr[i]);
+                i++;
             }
+            
+            printf("\n");
         }
     }
 }
 
 
-int load_matrix(long int **addr, const char *path, int N) {
+int load_matrix(struct Matrix *M, int N) {
     key_t key;
-    int id;
-    int size = N * N * sizeof(long int);
+    int size = N * N * sizeof(long);
     int flag = O_CREAT | 0644;
 
-    if ((key = ftok(path, 'R')) == -1) {
+    if ((key = ftok(M->path, 'A')) == -1) {
         perror("ftok");
         return -1;
     }
-
-    if ((id = shmget(key, size, flag)) == -1) {
+    
+    printf("---INFO\n");
+    printf("key: %x\tsize: %i\tflag: %i\n", key, size, flag);
+    if ((M->shmid = shmget(key, size, flag)) == -1) {
         perror("shmget");
         return -1;
     }
-
-    int fd;
     
-    if ((fd = open(path, O_RDONLY, S_IRUSR)) == -1) {
+    if ((M->fd = open(M->path, O_RDONLY, S_IRUSR)) == -1) {
         perror("open");
         return -1;
     }
 
-    if ((*addr = (long int *) shmat(id, NULL, 0)) == -1) {
+    if (*(M->shmaddr = (long *) shmat(M->shmid, NULL, 0)) == -1) {
         perror("shmat");
         return -1;
     }
+    
+    printf("---INFO2\n");
+    printf("path: %s\nkey: %x\nid: %i\nfd: %i\naddr: %li\n", M->path, key, M->shmid, M->fd, *M->shmaddr);
 
-    parse_matrix(addr, fd);
+    parse_matrix(M);
 
     return 0;
 }
+
+void sigint_handler(int sig, long **addr, ...) {
+    va_list ptr;
+    long **curr;
+    va_start(ptr, addr);
+    for (curr = va_arg(ptr, long **); curr != NULL; curr = va_arg(ptr, long**)) {
+        if (shmdt(curr) == -1) {
+            perror("shmdt");
+            exit(1);
+        }
+    }
+    exit(sig);
+}
+
+
 
 
 int main(int argc, char **argv) {
 
     // Leggi A e B
     int opt;
-
-    char *mA, *mB, *mC;
-    long int *mA_addr, *mB_addr, *mC_addr;
+    
+    struct Matrix A, B, C;
     int N;
     int P;
 
@@ -93,15 +123,15 @@ int main(int argc, char **argv) {
         if (opt == -1) break;
         switch (opt) {
             case 'A': 
-                    mA = optarg;
+                    A.path = optarg;
                 break;
 
             case 'B':
-                    mB = optarg;
+                    B.path = optarg;
                 break;
 
             case 'C':
-                    mC = optarg;
+                    C.path = optarg;
                 break;
 
             case 'N':
@@ -122,14 +152,24 @@ int main(int argc, char **argv) {
                 exit(1);
         }
     }
-    
-    if (load_matrix(&mA_addr, mA, &N) == -1) {
+   
+    printf("Loading matrix\n");
+
+    if (load_matrix(&A, N) == -1) {
         perror("load_matrix");
         exit(1);
     }
 
-    if (load_matrix(&mB_addr, mB, &N) == -1) {
+    if (load_matrix(&B, N) == -1) {
         perror("load_matrix");
         exit(1);
     }
+
+/* l'idea è di gestire un eventuale CTRL-C per eliminare anche le shm
+ * come ?
+
+    signal(SIGINT, (void (*)(int))sigint_handler);
+    sigint_handler(SIGINT, &mA_addr, &mB_addr, NULL);
+
+*/
 }
