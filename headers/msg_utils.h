142cf7228acc25f9bab8bcef5a9b3d544bbcd5d9
NoeMurr <noe.murr@outlook.com>
2017-05-12 20:53:39 +0200

    creati tipi di dato per la trasmissione dei messaggi

--- headers/msg_utils.h
+++ headers/msg_utils.h
@@ -1,5 +1,7 @@
-/*
- * file che conterrà tutte le funzioni ed i dati di
+/*! \file msg_utils.h
+ * Raccolta di funzioni e strutture dati per lo scambio di messaggi.
+ * 
+ * Conterrà tutte le funzioni ed i dati di
  * utilità per i messaggi scambiati tra padre e figli 
  * o viceversa attraverso code di messaggi e pipe
  */
@@ -7,17 +9,46 @@
 #ifndef MSG_UTILS_H
 #define MSG_UTILS_H
 #include <stdbool.h>
+#include <stdint.h>
 
+/*!
+ * Rappresenta il ruolo di un figlio.
+ * 
+ * Il ruolo è variabile ed è parte di un comando 
+ * che può essere inviato da un padre ad un figlio.
+ * Può essere:
+ * 	- MULTIPLY: il figlio deve effettuare una moltiplicazione.
+ * 	- SUM : il figlio deve effettuare una somma.
+ */
 typedef enum{
-	MULTIPLY,
+	MULTIPLY, 
 	SUM
 }role_t;
 
+/*!
+ * Rappresenta un comando inviato dal padre ad uno dei suoi figli.
+ * 
+ */
 typedef struct{
-	long type;
-	pid_t sender_pid;
-	bool success;
-	role_t role;
+	role_t role; //!< Ruolo che il figlio deve svolgere;
+	union data {
+		struct point{
+			uint32_t i;	
+			uint32_t j;
+		} c;
+		uint64_t row;
+	} x; //!< Dati del comando, in caso di somma va tenuto conto della
+	     //! valore di row, altrimenti va tenuto conto del valore di c.
+}cmd_t;
+
+/*!
+ * Rappresenta un messaggio per la coda di messaggi utilizzata dai 
+ * figli per comunicare con il padre.
+ */
+typedef struct{
+	long type;			//!< Tipo del messaggio.
+	bool success;		//!< True se il comando è stato eseguito correttamente.
+	cmd_t cmd;			//!< Comando che doveva essere eseguito.
 }msg_t;
 
 #endif
