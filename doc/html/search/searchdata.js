bfdb51c52e356d141aa45927fffa95b9274dd588
Mirko Morati <mirkomorati@gmail.com>
2017-05-12 15:08:14 +0200

    Aggiunto doxyfile e generate doc

---
+++ doc/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "est",
+  1: "s",
+  2: "et"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "Tutto",
+  1: "Strutture dati",
+  2: "Pagine"
+};
+
