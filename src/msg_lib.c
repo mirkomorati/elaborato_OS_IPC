a4d269b3b69bdb3dce57b37d92bf0ca77bcad011
NoeMurr <noe.murr@outlook.com>
2017-05-14 19:07:19 +0200

    risolti due piccoli problemi dovuti al copia incolla

--- src/msg_lib.c
+++ src/msg_lib.c
@@ -50,7 +50,7 @@ int send_msg(const msg_t * restrict msg, const int * restrict id){
 	if (msg != NULL){
 		sem_lock(sem_id);
 		size_t size = sizeof(msg_t) - sizeof(long);
-		if(msgsnd(msg_queue_id, msg, size) == -1){
+		if(msgsnd(msg_queue_id, msg, size, 0) == -1){
 			perror("sending msg: ");
 			sem_unlock(sem_id);
 			return -1;
@@ -65,7 +65,7 @@ int rcv_msg(msg_t * restrict msg, const int * restrict id){
 	
 	if (id != NULL) msg_queue_id = *id;
 
-	if (cmd != NULL){
+	if (msg != NULL){
 		size_t size = sizeof(msg_t) - sizeof(long);
 		if (msgrcv(msg_queue_id, msg, size, 0, 0)){
 			// N.B. leggo sempre il tipo 0 perché non penso ci 
