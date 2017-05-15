721e3fd18791fa538245e17c3cb50df81320e24c
NoeMurr <noe.murr@outlook.com>
2017-05-15 23:03:08 +0200

    cambiato l'ordine delle funzioni nel file col main

--- src/father.c
+++ src/father.c
@@ -1,5 +1,93 @@
 #include "../headers/father.h"
 
+int main(int argc, char **argv) {
+    /*! 
+     * \struct Struttura da passare a getopt() per l'alias delle opzioni
+     */
+    static struct option long_opt[] = {
+        {"matrixA",   required_argument, 0, 'A'},
+        {"matrixB",   required_argument, 0, 'B'},
+        {"matrixC",   required_argument, 0, 'C'},
+        {"order",     required_argument, 0, 'N'},
+        {"processes", required_argument, 0, 'P'},
+        {"help",      no_argument,       0, 'h'},
+        {0,           0,                 0,  0 }
+    };
+
+    int opt;
+    char *short_opt = "A:B:C:N:P:h";
+
+    shm_t A, B, C;
+    shm_t S;
+    int N;
+    int P;
+
+    if (argc < 10) {
+        char *buf = "Error: too few arguments.\nusage: ./elaborato_IPC -A matrix -B matrix -C matrix -N order -P #processes\n";
+        write(STDOUT, buf, sizeof(char) * strlen(buf));
+        exit(1);
+    }
+
+    while ((opt = getopt_long(argc, argv, short_opt, long_opt, NULL)) != -1) {
+        if (opt == -1) break;
+        switch (opt) {
+            case 'A': 
+                A.path = optarg;
+                break;
+
+            case 'B':
+                B.path = optarg;
+                break;
+
+            case 'C':{
+                // controllo se esiste il file altrimenti lo creo.
+                struct stat st;
+                if(stat(optarg, &st) == -1 && errno == ENOENT)
+                    if(creat(optarg, S_IRUSR) == -1)
+                        perror("Error creating matrixC file: ");
+                C.path = optarg;
+                break;
+            }
+            case 'N':
+                N = atoi(optarg);
+                break;
+            
+            case 'P':
+                P = atoi(optarg);
+                break;
+
+            case 'h': {
+                char *buf = "usage: ./elaborato_IPC -A matrix -B matrix -C matrix -N order -P #processes\n";
+                write(STDOUT, buf, sizeof(char) * strlen(buf));
+                break;
+            }
+            default:
+                printf("Wrong arguments\n");
+                printf("Usage: \n");
+                exit(1);
+        }
+    }
+    A.N = N;
+    B.N = N;
+    C.N = N;
+    S.N = 1;
+
+    shm_t *shm_array[5] = {&A, &B, &C, &S, NULL};
+    int pipe_fd, queue_id;
+
+    if(init(shm_array) == -1) {
+        perror("init");
+        sig_end(-1);
+    }
+
+    if (make_child(shm_array, P, &pipe_fd, &queue_id) == -1){
+        perror("make_child");
+        sig_end(-1);
+    }
+
+    sig_end(run(P, pipe_fd, queue_id));
+}
+
 int init(shm_t **shm_array) {
 #ifdef DEBUG   
     printf("Loading matrix\n");
@@ -94,92 +182,4 @@ int make_child(shm_t **shm_array , int P, int *pipe_fd, int *queue_id) {
 
 int run(int P, int pipe, int queue) {
     return 0;
-}
-
-int main(int argc, char **argv) {
-    /*! 
-     * \struct Struttura da passare a getopt() per l'alias delle opzioni
-     */
-    static struct option long_opt[] = {
-        {"matrixA",   required_argument, 0, 'A'},
-        {"matrixB",   required_argument, 0, 'B'},
-        {"matrixC",   required_argument, 0, 'C'},
-        {"order",     required_argument, 0, 'N'},
-        {"processes", required_argument, 0, 'P'},
-        {"help",      no_argument,       0, 'h'},
-        {0,           0,                 0,  0 }
-    };
-
-    int opt;
-    char *short_opt = "A:B:C:N:P:h";
-
-    shm_t A, B, C;
-    shm_t S;
-    int N;
-    int P;
-
-    if (argc < 10) {
-        char *buf = "Error: too few arguments.\nusage: ./elaborato_IPC -A matrix -B matrix -C matrix -N order -P #processes\n";
-        write(STDOUT, buf, sizeof(char) * strlen(buf));
-        exit(1);
-    }
-
-    while ((opt = getopt_long(argc, argv, short_opt, long_opt, NULL)) != -1) {
-        if (opt == -1) break;
-        switch (opt) {
-            case 'A': 
-                A.path = optarg;
-                break;
-
-            case 'B':
-                B.path = optarg;
-                break;
-
-            case 'C':{
-                // controllo se esiste il file altrimenti lo creo.
-                struct stat st;
-                if(stat(optarg, &st) == -1 && errno == ENOENT)
-                    if(creat(optarg, S_IRUSR) == -1)
-                        perror("Error creating matrixC file: ");
-                C.path = optarg;
-                break;
-            }
-            case 'N':
-                N = atoi(optarg);
-                break;
-            
-            case 'P':
-                P = atoi(optarg);
-                break;
-
-            case 'h': {
-                char *buf = "usage: ./elaborato_IPC -A matrix -B matrix -C matrix -N order -P #processes\n";
-                write(STDOUT, buf, sizeof(char) * strlen(buf));
-                break;
-            }
-            default:
-                printf("Wrong arguments\n");
-                printf("Usage: \n");
-                exit(1);
-        }
-    }
-    A.N = N;
-    B.N = N;
-    C.N = N;
-    S.N = 1;
-
-    shm_t *shm_array[5] = {&A, &B, &C, &S, NULL};
-    int pipe_fd, queue_id;
-
-    if(init(shm_array) == -1) {
-        perror("init");
-        sig_end(-1);
-    }
-
-    if (make_child(shm_array, P, &pipe_fd, &queue_id) == -1){
-        perror("make_child");
-        sig_end(-1);
-    }
-
-    sig_end(run(P, pipe_fd, queue_id));
 }
