4a5d448b7b1b03dbc16bf4791dd5d07672532463
Mirko Morati <mirkomorati@gmail.com>
2017-05-24 13:17:39 +0200

    qualche prova

--- headers/io_lib.h
+++ headers/io_lib.h
@@ -1,5 +1,6 @@
-/*!
- * \file io_lib.h
+/**
+ * @file io_lib.h
+ * Funzioni utili per l'I/O che utilizzano syscall
  */
 
 #ifndef IO_LIB_H
@@ -10,8 +11,21 @@
 #define BUF_SIZE 4096
 
 
+/**
+ * @brief      Funzione che simula il comportamento della printf usando
+ *             la syscall write
+ *
+ * @param[in]  fd         File descriptor su cui scrivere
+ * @param      str        Stringa di formattazione
+ * @param[in]  ...        Argomenti da sostituire
+ */
 void sys_print(int fd, char *str, ...);
 
+/**
+ * @brief      Funzione che simula il comportamento della perror
+ *
+ * @param      str   La stringa da stampare come errore
+ */
 void sys_err(char *str);
 
 #endif
