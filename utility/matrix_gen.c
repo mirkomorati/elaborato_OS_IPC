11d7c8e12b212c786412d269b28a3c49ba4f622a
Mirko Morati <mirkomorati@gmail.com>
2017-06-21 23:20:53 +0200

    Le vie del signore sono infinite. Soprattutto sono più di 32. La 48 è l'unica vostra salvezza. Non usate mai shared memory.

--- utility/matrix_gen.c
+++ utility/matrix_gen.c
@@ -3,32 +3,43 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <time.h>
+#include <string.h>
 
 #define MAX_RAND 10
 #define OFFSET -5
+
+// argv: pathname minN maxN
 
 int main(int argc, char **argv) {
     int fd;
 
     srand(time(NULL));
-
-    if ((fd = open(argv[1], O_WRONLY | O_CREAT | O_EXCL, 0644)) <= 0) {
-        if ((fd = open(argv[1], O_WRONLY | O_EXCL | O_TRUNC)) == -1) {
-            perror("open");
-            exit(1);
-        }
-    }
-
-    printf("generating %s, range %s\n", argv[1], argv[2]);
+    if (argc < 4)
+        printf("Error\n");
 
     char buffer[20];
-    int N = atoi(argv[2]); 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            int bytes = sprintf(buffer, (j + 1 == N ? "%d\n" : "%d,"), rand() % MAX_RAND + OFFSET);
-            write(fd, buffer, bytes);
+    int N = atoi(argv[2]);
+    int maxN = atoi(argv[3]);
+
+    for (int l = N; l <= maxN; l++) {
+        char path[255];
+        sprintf(path, "%s%d", argv[1], l);
+        if ((fd = open(path, O_WRONLY | O_CREAT | O_EXCL, 0644)) <= 0) {
+            if ((fd = open(path, O_WRONLY | O_EXCL | O_TRUNC)) == -1) {
+                perror("open");
+                exit(1);
+            }
+        }
+
+        printf("generating %s, order %i\n", path, l);
+
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < l; j++) {
+                int bytes = sprintf(buffer, (j + 1 == l ? "%d\n" : "%d,"), rand() % MAX_RAND + OFFSET);
+                write(fd, buffer, bytes);
+            }
         }
     }
-    
+
     return 0;
 }
--- utility/test.sh
+++ utility/test.sh
@@ -3,7 +3,26 @@
 lower=$1
 upper=$2
 P=$3
+matlab_cmd=""
 
 for i in `seq $lower $upper`; do
-    ../elaborato_IPC -A ../../sample/m_in/matrixA$i -B ../../sample/m_in/matrixB$i -C ../../sample/m_out/matrixC$i -N $i -P $P
+    echo -e "\n======================"
+    echo "ELABORATO ORDER $i"
+    echo
+    ../elaborato_IPC -A ../../sample/m_in/matrixA$i -B ../../sample/m_in/matrixB$i -C ../../sample/m_out/matrixC$i -N $i -P $P > /dev/null
+    matlab_cmd="$matlab_cmd A=csvread('matrixA$i');B=csvread('matrixB$i');csvwrite('../m_out/matlab_matrixC$i', A*B);"
+done
+
+matlab_cmd="$matlab_cmd exit;"
+
+echo -e "\n======================"
+echo "MATLAB WORKING"
+/Applications/MATLAB_R2017a.app/bin/matlab -nodesktop -nosplash -nojvm -sd ../../sample/m_in -r "$matlab_cmd" > /dev/null
+echo
+
+for i in `seq $lower $upper`; do
+    echo -e "\n======================"
+    echo "DIFF ORDER $i"
+    ./matrix_diff ../../sample/m_out/matrixC$i ../../sample/m_out/matlab_matrixC$i $i
+    echo
 done
