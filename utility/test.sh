a488c8d7f80ee446088a48cc7734e6db1026ce2d
NoeMurr <noe.murr@outlook.com>
2017-06-22 19:09:29 +0200

    aggiunta riga al .gitignore per non caricare i sample e risolto problemi con il file di test per i path

--- utility/test.sh
+++ utility/test.sh
@@ -10,14 +10,14 @@ for i in `seq $lower $upper`; do
     echo "ELABORATO ORDER $i"
     echo
     ../elaborato_IPC -A ../../sample/m_in/matrixA$i -B ../../sample/m_in/matrixB$i -C ../../sample/m_out/matrixC$i -N $i -P $P > /dev/null
-    matlab_cmd="$matlab_cmd A=csvread('matrixA$i');B=csvread('matrixB$i');csvwrite('../m_out/matlab_matrixC$i', A*B);"
+    matlab_cmd="$matlab_cmd A=csvread('../../sample/m_in/matrixA$i');B=csvread('../../sample/m_in/matrixB$i');csvwrite('../../sample/m_out/matlab_matrixC$i', A*B);"
 done
 
 matlab_cmd="$matlab_cmd exit;"
 
 echo -e "\n======================"
 echo "MATLAB WORKING"
-/Applications/MATLAB_R2017a.app/bin/matlab -nodesktop -nosplash -nojvm -sd ../../sample/m_in -r "$matlab_cmd" > /dev/null
+/Applications/MATLAB_R2016b.app/bin/matlab -nodesktop -nosplash -nojvm -sd ../../sample/m_in -r "$matlab_cmd" > /dev/null
 echo
 
 for i in `seq $lower $upper`; do
